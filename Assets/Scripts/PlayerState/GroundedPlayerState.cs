e1c7bc53fc46e6b35294d43fe01ae61af8c16eb5
EnricoCosentino <enrico-cosentino@libero.it>
2023-02-04 10:54:56 +0100

    Aggiunti i canceling di salto e movimento in salto

--- Assets/Scripts/PlayerState/GroundedPlayerState.cs
+++ Assets/Scripts/PlayerState/GroundedPlayerState.cs
@@ -27,7 +27,7 @@ public class GroundedPlayerState : AbstractPlayerState
     public override void PretendFixedUpdate() { 
         Vector3 currentVelocity = playerBody.velocity;
 
-        Vector3 targetVelocity = new Vector3(moveValue.x, 0, moveValue.y) * Speed;
+        Vector3 targetVelocity = new Vector3(moveValue.x, currentVelocity.y, moveValue.y) * Speed;
 
         if(moveValue.magnitude > 0) {
             GameObject camera = GameObject.Find("Main Camera");
--- Assets/Scripts/PlayerState/JumpPlayerState.cs
+++ Assets/Scripts/PlayerState/JumpPlayerState.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class JumpPlayerState : AbstractPlayerState
 {
-    public float JumpForce { get; set; } = 4f;
+    public float JumpForce { get; set; } = 10f;
     private bool doubleJump = false;
+    private bool cancelJump = false;
     public float Acceleration { get; set; } = 3f;
 
     public override void HandleMove(InputAction.CallbackContext ctx, GameObject player)
@@ -17,20 +19,41 @@ public class JumpPlayerState : AbstractPlayerState
     {
         if(ctx.performed)
         {
+            Debug.Log("Jump performed");
             doubleJump = true;
+        }
+        if(ctx.canceled)
+        {
+            cancelJump = true;
+            Debug.Log("Jump canceled");
         }
     }
     public override void HandleAttack(InputAction.CallbackContext ctx, GameObject player) { }
 
     public override void PretendFixedUpdate() {
-        Vector3 accelerationForce = new Vector3(moveValue.x, 0, moveValue.y) * Acceleration;
-        playerBody.AddForce(accelerationForce, ForceMode.Force);
+        if (moveValue.sqrMagnitude != 0)
+        {
+            Vector3 accelerationForce = new Vector3(moveValue.x, 0, moveValue.y) * Acceleration;
+            playerBody.AddForce(accelerationForce, ForceMode.Force);
+        } 
+        else
+        {
+            playerBody.velocity = new Vector3(0, playerBody.velocity.y, 0);
+        }
 
         if (doubleJump)
         {
             doubleJump = false;
             playerBody.AddForce(0, JumpForce, 0, ForceMode.Impulse);
             context.SetPlayerState("DOUBLEJUMP");
+            return;
+        }
+
+        if (cancelJump && player.transform.position.y >= 4f)
+        {
+            cancelJump = false;
+            playerBody.velocity = new Vector3(playerBody.velocity.x, 0, playerBody.velocity.z);
+            return;
         }
     }
 
